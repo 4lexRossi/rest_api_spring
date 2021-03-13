eba5e0f4de26fc26129618c1a4349d6074e711d0
4lexRossi <devalexrossi@gmail.com>
2021-03-13 19:09:03 -0300

    initial settings and client model creations

---
+++ src/main/java/com/alostech/api/AlosTechApiApplication.java
@@ -0,0 +1,13 @@
+package com.alostech.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlosTechApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlosTechApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alostech/api/AlosTechApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alostech.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlosTechApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
