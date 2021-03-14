3b798162ed6a463bceda890839b93e45f30598bd
4lexRossi <devalexrossi@gmail.com>
2021-03-14 18:03:51 -0300

    Conection to MySQL and Flyway configuration

--- src/main/java/com/alostech/domain/model/Client.java
+++ src/main/java/com/alostech/domain/model/Client.java
@@ -1,11 +1,20 @@
 package com.alostech.domain.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Client {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String name;
 	private String email;
-	private String telefone;
+	private String telephone;
 	
 	public Long getId() {
 		return id;
@@ -31,12 +40,37 @@ public class Client {
 		this.email = email;
 	}
 	
-	public String getTelefone() {
-		return telefone;
+	public String getTelephone() {
+		return telephone;
 	}
 	
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}	
 
 }
